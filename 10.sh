5841f1b02f89035bfa1b54d1fb92ee3d0714d477
oi10536 <34789129+oi10536@users.noreply.github.com>
2018-01-25 09:49:13 +0700

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -2,7 +2,8 @@
 #About this script
 
 clear
-echo -e "Script Auto Install SSH & OpenVPN v1.0" 
+figlet "TONZA-VPN"
+echo -e "Script Auto Install SSH & OpenVPN v2.1" 
 echo -e ""
 echo -e "For Debian 7 , 8 32 bit & 64 bit"
 echo -e "For VPS with KVM PuTTY and VMWare virtualization"
