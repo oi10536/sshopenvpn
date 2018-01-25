7a84679cdf9933dd115f458d160256e7a4fbe804
oi10536 <34789129+oi10536@users.noreply.github.com>
2018-01-25 09:33:45 +0700

    Add files via upload

---
+++ FREESCRITP.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+#
+# Script by TONZA-VPN
+# 
+# ==================================================
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# go to root
+cd
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Bangkok/etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# set repo
+wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/sources.list.debian7"
+wget "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/dotdeb.gpg"
+wget "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/jcameron-key.asc"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
+
+# update
+apt-get update
+
+# install webserver
+apt-get -y install nginx
+
+# install essential package
+apt-get -y install nano iptables dnsutils openvpn screen whois ngrep unzip unrar
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
+curl -L "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/bintray-public.key.asc" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
+apt-get update
+apt-get install neofetch
+
+echo "clear" >> .bashrc
+echo 'echo -e " ยินดีต้อนรับสู่ Server $HOSTNAME"' >> .bashrc
+echo 'echo -e "Script by TONZA-VPN "' >> .bashrc
+echo 'echo -e "กรุณาพิมพ์   MENU เพื่อแสดงรายการคำสั่ง "' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Setup by Bustami Arifin</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/vps.conf"
+service nginx restart
+
+# install openvpn
+wget -O /etc/openvpn/openvpn.tar "https://github.com/oi10536/sshopenvpn/raw/master/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/1194.conf"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables_yg_baru_dibikin.conf
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/iptables"
+chmod +x /etc/network/if-up.d/iptables
+service openvpn restart
+
+# konfigurasi openvpn
+cd /etc/openvpn/
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/open-vpn.conf"
+sed -i $MYIP2 /etc/openvpn/client.ovpn;
+cp client.ovpn /home/vps/public_html/
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://github.com/oi10536/sshopenvpn/raw/master/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://github.com/oi10536/sshopenvpn/raw/master/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+# setting port ssh
+cd
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+service ssh restart
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+# install squid3
+cd
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/squid.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
+
+# install webmin
+cd
+wget -O webmin-current.deb "http://www.webmin.com/download/deb/webmin-current.deb"
+dpkg -i --force-all webmin-current.deb;
+apt-get -y -f install;
+rm /root/webmin-current.deb
+service webmin restart
+
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/menu.sh"
+wget -O 1 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/1.sh"
+wget -O 2 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/2.sh"
+wget -O 3 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/3.sh"
+wget -O 4 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/4.sh"
+wget -O 5 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/5.sh"
+wget -O 6 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/6.sh"
+wget -O 7 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/7.sh"
+wget -O 8 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/8.sh"
+wget -O 9 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/9.sh"
+wget -O 10 "https://raw.githubusercontent.com/oi10536/sshopenvpn/master/10.sh"
+
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+
+chmod +x menu
+chmod +x 1
+chmod +x 2
+chmod +x 3
+chmod +x 4
+chmod +x 5
+chmod +x 6
+chmod +x 7
+sed -i -e 's/\r$//' 7
+chmod +x 8
+chmod +x 9
+chmod +x 10
+
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+service nginx start
+service openvpn restart
+service cron restart
+service ssh restart
+service dropbear restart
+service squid3 restart
+service webmin restart
+rm -rf ~/.bash_history && history -c
+echo " เพื่อยกเลิกการติดตั้ง " >> /etc/profile
+
+# info
+clear
+echo " Autoscript VPN " | tee log-install.txt
+echo "===========================================" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Service VPN"  | tee -a log-install.txt
+echo "-------"  | tee -a log-install.txt
+echo "OpenSSH  : 22, 143"  | tee -a log-install.txt
+echo "Dropbear : 80, 443"  | tee -a log-install.txt
+echo "Squid3   : 8080, 3128 (limit to IP SSH)"  | tee -a log-install.txt
+echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:81/client.ovpn)"  | tee -a log-install.txt
+echo "badvpn   : badvpn-udpgw port 7300"  | tee -a log-install.txt
+echo "nginx    : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Script"  | tee -a log-install.txt
+echo "------"  | tee -a log-install.txt
+echo         "พิมพ์  MENU (เพื่อใช้คำสั่งต่างๆ ใน script)"  | tee -a log-install.txt
+echo "===========================================" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo " คุณสมบัติอื่น ๆ "  | tee -a log-install.txt
+echo "----------"  | tee -a log-install.txt
+echo "Webmin   : http://$MYIP:10000/"  | tee -a log-install.txt
+echo "Timezone : Asia/Bangkok (GMT +7)"  | tee -a log-install.txt
+echo "IPv6     : [off]"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo                "BY MAZE MASTER"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo " ติดตั้งสำเร็จ  log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo " VPS AUTO REBOOT 12 TIME "  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "==========================================="  | tee -a log-install.txt
+cd
+rm -f /root/FREESCRITP.sh
